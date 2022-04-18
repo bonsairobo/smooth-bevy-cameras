69b0b93f549f601f5d1623e75ead4ba626a0c185
David Taralla <davidtaralla@gmail.com>
2022-04-18 11:57:32 -0400

    Coding style

--- examples/simple_look_transform.rs
+++ examples/simple_look_transform.rs
@@ -39,12 +39,15 @@ fn setup(
 
     commands
         .spawn_bundle(LookTransformBundle {
-            transform: LookTransform::new(Vec3::new(-2.0, 2.5, 5.0), Vec3::new(0.0, 0.5, 0.0)),
+            transform: LookTransform {
+                eye: Vec3::new(-2.0, 2.5, 5.0),
+                target: Vec3::new(0.0, 0.5, 0.0),
+            },
             smoother: Smoother::new(0.9),
         })
         .insert_bundle(PerspectiveCameraBundle {
             transform: Transform::from_xyz(-2.0, 2.5, 5.0)
                 .looking_at(Vec3::new(0.0, 0.5, 0.0), Vec3::Y),
-            ..Default::default()
+            ..default()
         });
 }
