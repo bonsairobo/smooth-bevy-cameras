335638e8ae33dc520676c91b5045cc06600f25e7
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2024-02-19 13:38:27 -0800

    install rust toolchain via Nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
@@ -14,19 +14,69 @@
   in {
     devShell.${system} = with pkgs;
       mkShell rec {
-        # Rust binaries need to be able to link and load some system libraries.
-        buildInputs = [
+        ###
+        ## Packages
+        ###
+
+        xLibs = with pkgs; [
           libxkbcommon
-          pkg-config
-          udev
-          vulkan-loader
           xorg.libX11
           xorg.libXcursor
           xorg.libXi
           xorg.libXrandr
-          wayland
         ];
-        LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        dynLibs = with pkgs;
+          [
+            alsa-lib
+            stdenv.cc.cc.lib
+            udev
+            vulkan-loader
+          ]
+          ++ xLibs;
+
+        buildInputs = with pkgs;
+          [
+            clang
+            llvmPackages_16.bintools
+            mold
+            pkg-config
+            rustup
+            yq-go
+          ]
+          ++ dynLibs;
+
+        ###
+        ## Rust Toolchain Setup
+        ###
+
+        shellHook = ''
+          export RUSTC_VERSION=$(yq ".toolchain.channel" rust-toolchain.toml)
+          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+          rustup target add wasm32-unknown-unknown
+          rustup component add rust-analyzer
+        '';
+
+        ###
+        ## Rust Bindgen Setup
+        ###
+
+        # So bindgen can find libclang.so
+        LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_16.libclang.lib];
+        # Add headers to bindgen search path
+        BINDGEN_EXTRA_CLANG_ARGS = [
+          ''-I"${pkgs.llvmPackages_16.libclang.lib}/lib/clang/${pkgs.llvmPackages_16.libclang.version}/include"''
+        ];
+
+        ###
+        ## Linking with System libraries
+        ###
+
+        # Add precompiled library to rustc search path
+        RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') dynLibs;
+
+        # Some libraries must be in the dynamic linker path.
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libxkbcommon pkgs.vulkan-loader];
       };
   };
 }
